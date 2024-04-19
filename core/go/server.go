a37df9168943b7ea7233b9bcd784420ca1dae29c
Hotweels94 <ryvida@hotmail.fr>
2024-04-19 16:22:55 +0200

    first html page link to go server

--- core/go/server.go
+++ core/go/server.go
@@ -2,19 +2,35 @@ package forum
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
+	"strconv"
 )
 
+type mainInfo struct {
+	PageName      string
+	NumberOfVisit int
+}
+
 type web struct {
-	test string
+	Test string
 }
 
 func (web *web) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	web.test = "Forum Nico Ryan"
-	fmt.Fprintf(w, web.test)
+	web.Test = "Forum Nico Ryan"
+	fmt.Fprintf(w, web.Test)
+}
+
+func (m *mainInfo) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	m.NumberOfVisit++
+	m.PageName = "Forum Jeux video !! There are : " + strconv.Itoa(m.NumberOfVisit) + " visits !!"
+	t, _ := template.ParseFiles("src/html/index.html")
+	t.Execute(w, m)
+
 }
 
 func HandleTest() {
+	http.Handle("/", new(mainInfo))
 	http.Handle("/test", new(web))
 	http.ListenAndServe(":8080", nil)
 }
